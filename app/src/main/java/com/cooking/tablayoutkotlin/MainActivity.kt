b7e44de31187eca6a6cfb661aec8bad44bd65e04
weiwei <tyler22501789@gmail.com>
2020-11-22 22:11:35 +0800

    #updated 20201122 10:10 PM #BottonNavigation #Kotlin

---
+++ app/src/main/java/com/cooking/tablayoutkotlin/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.cooking.tablayoutkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.cooking.tablayoutkotlin.fragments.CameraFragment
+import com.cooking.tablayoutkotlin.fragments.FavoritesFragment
+import com.cooking.tablayoutkotlin.fragments.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val cameraFragment = CameraFragment()
+        val favoritesFragment = FavoritesFragment()
+
+        setCurrentFragment(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.nav_home -> setCurrentFragment(homeFragment)
+                R.id.nav_camera -> setCurrentFragment(cameraFragment)
+                R.id.nav_favorites -> setCurrentFragment(favoritesFragment)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+}
